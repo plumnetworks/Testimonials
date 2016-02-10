d88986e8ef498a4ed508a7c08e195e8793e45548
backuser <backuser@linux-6pwl.site>
2016-02-10 10:19:42 +0300

    fixed

--- app/code/local/Creovector/Testimonials/Model/Mysql4/Wbxtest/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Creovector_Testimonials_Model_Mysql4_Wbxtest_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract {
-
-    public function _construct() {
-        $this->_init("wbxtest/wbxtest");
-    }
-
-}
--- app/code/local/Creovector/Testimonials/Model/Resource/Setup.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Creovector_Testimonials_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup {
-    
-}
-
-?>
